46885b9435c68596362e6d4aa4c8b60b455a67ee
lulhi <michaelborov2007@gmail.com>
2022-09-16 17:56:49 -0700

    -v

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   mode: "jit",
   content: ["./**/*.html"],
   theme: {
+    colors:{
+      'light-blue': '#8AB6F8',
+      'dark-blue': '#0E437A',
+    },
     extend: {},
   },
   plugins: [],
