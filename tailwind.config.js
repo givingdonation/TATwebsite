c13913357048020f2b27867cf58fef85153b6c93
lulhi <michaelborov2007@gmail.com>
2022-09-17 21:19:28 -0700

    Quick Prayer

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,21 @@ module.exports = {
   mode: "jit",
   content: ["./**/*.html"],
   theme: {
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
     colors:{
       'light-blue': '#8AB6F8',
       'dark-blue': '#0E437A',
+      'grey': '#FFFFFF',
+      'orange': '#cd7a00',
+      'black': '#432800'
+    },
+    fontFamily: {
+      lora: [ "lora", "sans-serif" ],
     },
     extend: {},
   },
